172c5ee1d3b89d0815e12937c3212e127caba8de
mohd.anas <anasstudy01@gmail.com>
2024-03-28 01:47:21 +0530

    stings and method

---
+++ 07heapandstack.js
@@ -0,0 +1,31 @@
+//there are 2 types of memory   stack and heap 
+// stack is used for premitive datatyeps   
+// heap is used for reference  non premitive datatyeps
+let coldrink1 ="cocakola"
+let  coldrink2 = coldrink1;
+coldrink2 ="mountaindew"
+
+
+
+
+console.log(coldrink1);
+console.log(coldrink2);
+
+
+let userOne = {
+    email:"john@gmail.com",
+    password :2334343,
+    upiid :"user@ybl"
+}
+
+let user2 =userOne;
+user2.email="jane@gmail.com";
+
+console.log("User one ",userOne);
+console.log("User two ",user2);
+
+
+
+
+
+
