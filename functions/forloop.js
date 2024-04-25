b2cf6d1d6f40565f92f729de3badd4b715a99f3a
mohd.anas <anasstudy01@gmail.com>
2024-04-25 15:22:05 +0530

    dom completed

--- functions/forloop.js
+++ functions/forloop.js
@@ -6,9 +6,11 @@
 
 // all mathe matical table 1 to 10
 
-for (let i = 1; i <= 10; i++) {
-  console.log(`outer loop ${i}`);
-  for (let j = 1; j <= 10; j++) {
-    console.log(`${i} x ${j}  = ${i * j}`);
-  }
-}
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`outer loop ${i}`);
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} x ${j}  = ${i * j}`);
+//   }
+// }
+
+
